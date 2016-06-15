e72a726ba2fcbfe24fc6777a8905c1b7ed9c7dbf
omtinez <omtinez@gmail.com>
2016-06-14 23:14:07 -0700

    test requirements

--- setup.py
+++ setup.py
@@ -15,11 +15,9 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
 ]
 
 test_requirements = [
-    unittest2
 ]
 
 setup(
